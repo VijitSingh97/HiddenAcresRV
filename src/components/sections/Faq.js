65ebe005fdfcab583d2d88d29385a15d45b41e94
Vijit Singh <vijit.singh@aa.com>
2020-10-22 22:07:10 -0500

    update faq

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -20,7 +20,45 @@ const FAQS = [
     title: 'What are the dimension of the RV pads at Hidden Acres?',
     content: () => (
       <>
-        RV pads start at 30' L x 16' W, and our largest pads are 45' L x 16' W. Each unit also includes a 24' L x 20' W parking spot for two vehicles.
+        Our RV pad sizes are:
+        <ul>
+          <li>
+            25' L x 16' W
+          </li>
+          <li>
+            35' L x 16' W
+          </li>
+          <li>
+            45' L x 16' W
+          </li>
+        </ul>
+        Each lot also includes a 2 vehicle, 24' L x 20' W, parking spot in front of them. Longer RVs can utilize this additional space to increases the lot length by up to 24'.
+      </>
+    ),
+  },
+  {
+    title: 'What are the RV age Restrictions? Any other restrictions?',
+    content: () => (
+      <>
+      Our restrictions include:
+      <ul>
+        <li>
+          RV must be 15 years old or younger
+        </li>
+        <li>
+          No damage to the outside of the RV
+        </li>
+        <li>
+          RV must have safe and working electrical plugs
+        </li>
+        <li>
+          RVs must have undamaged and working water and sewage hook ups
+        </li>
+        <li>
+          Any window ACs and window fans must be approved by management
+        </li>
+      </ul>
+          More information can be found in our campground rules: <a href={parkRules}>campground rules</a>.
       </>
     ),
   },
