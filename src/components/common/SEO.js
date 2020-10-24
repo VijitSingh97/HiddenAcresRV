c5890b12bb3728860d6c9c66945f7f8152c2ee49
Vijit Singh <vijit.singh@aa.com>
2020-10-23 20:33:01 -0500

    update seo

--- src/components/common/SEO.js
+++ src/components/common/SEO.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 const SEO_DATA = {
-  description: 'Website for Hidden Acres RV Campground located in Princeton, TX',
-  title: 'Hidden Acres RV Campground',
-  url: '',
+  description: 'A quiet and friendly place to relax! Hidden Acres RV Park is located in Princeton, TX. On Lake Lavon, lush trees, with beach access, 20 min from Plano and McKinney!',
+  title: 'Hidden Acres RV Park',
+  url: 'https://www.hiddenacresrv.com',
   author: 'Vijit Singh',
-  keywords: ['Hidden Acres RV Campground', 'RV Park', 'Hidden Acres', 'RV Park Princeton', 'RV Park Lavon'],
-  img: '',
-  twitterId: '@DatTurban',
+  keywords: ['rv park near me', 'rv near me', 'rv park near princeton tx', 'rv park near lake lavon tx', 'rv park near mckinney tx', 'rv park near plano tx', 'rv park near frisco txt', 'rv park near dallas tx', 'campground near me'],
+  img: '../../images/logo/HiddenAcresRVCampgroundLogo1.png',
+  twitterId: '@Hidden_Acres_RV',
   facebookId: '',
 };
 
@@ -21,6 +21,7 @@ const SEO = () => {
       <meta property="og:url" content={SEO_DATA.url} />
       <meta property="og:image" content={SEO_DATA.img} />
       <meta property="og:description" content={SEO_DATA.description} />
+      <meta property="og:locale" content={SEO_DATA.locale} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={SEO_DATA.twitterId} />
@@ -33,6 +34,8 @@ const SEO = () => {
       <meta name="description" content={SEO_DATA.description} />
       <meta name="keywords" content={SEO_DATA.keywords.join(', ')} />
       <meta name="author" content={SEO_DATA.author} />
+
+      <link rel="canonical" href="https://www.hiddenacresrv.com"/>
       <title>{SEO_DATA.title}</title>
       <html lang="en" />
     </Helmet>
